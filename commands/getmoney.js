385e311fb48f850159fe1bdb81a461f7bb758b7d
시혁 박 <exon.indiv@gmail.com>
2020-12-25 23:26:12 +0900

    cooltime

--- commands/getmoney.js
+++ commands/getmoney.js
@@ -10,18 +10,27 @@ const connection = mysql.createConnection({
   database : settings.mysql.database
 })
 
+const cooltime = new Set()
+
 exports.run = (client, message, args) => {
+  
   let sql = `SELECT * FROM users WHERE id like '${message.author.id}'`
   connection.query(sql, (err, rows, fields) => {
     if (err) console.log(err)
     if (!rows.length) message.channel.send(new MessageEmbed().addField('가입 먼저 해', '`$가입` 하라고요'))
     else {
-      sql = `SELECT * FROM users WHERE id like '${message.author.id}'`
-      connection.query(sql, (err, rows, fields) => {
-        sql = `UPDATE users SET money = ${Number(rows[0].money) + 100} WHERE id = '${message.author.id}'`
-        connection.query(sql)
-        message.channel.send('돈 받음.\n현재 돈 : ' + (Number(rows[0].money) + 100))
-      })
+      if (!cooltime.has(message.author.id)) {
+        sql = `SELECT * FROM users WHERE id like '${message.author.id}'`
+        connection.query(sql, (err, rows, fields) => {
+          sql = `UPDATE users SET money = ${Number(rows[0].money) + 100} WHERE id = '${message.author.id}'`
+          connection.query(sql)
+          message.channel.send('돈 받음.\n현재 돈 : ' + (Number(rows[0].money) + 100))
+          cooltime.add(message.author.id)
+          setTimeout(() => {cooltime.delete(message.author.id)}, 15000)
+        })
+      } else {
+        message.channel.send('쿨타임임 좀만 기다리셈')
+      }
     }
   })
   
--- commands/register.js
+++ commands/register.js
@@ -15,7 +15,7 @@ exports.run = (client, message, args) => {
   connection.query(sql, (err, rows, fields) => {
     if (err) console.log(err)
     if (!rows.length) {
-      sql = `INSERT INTO users VALUES ('${message.author.id}', 10)`
+      sql = `INSERT INTO users VALUES ('${message.author.id}', 100)`
       connection.query(sql, (err, rows, fields) => { if (err) console.log(err) })
       message.channel.send(new MessageEmbed().addField('가입 완료', '가입 완료함 ㅅㄱ'))
     } else message.channel.send(new MessageEmbed().addField('가입 이미했', '가입 이미했자나'))
