76e1c925e63367a469e81c552369110b893688e9
시혁 박 <exon.indiv@gmail.com>
2020-12-25 22:46:20 +0900

    gambling

--- commands/gambling.js
+++ commands/gambling.js
@@ -11,20 +11,26 @@ const connection = mysql.createConnection({
 })
 
 exports.run = (client, message, args) => {
-
   let sql = `SELECT * FROM users WHERE id like '${message.author.id}'`
   connection.query(sql, (err, rows, fields) => {
-    if (Math.floor(Math.random() * Math.floor(2))) {
-      message.channel.send('도박 성공 ㅅㄱ 남은 돈: ' + rows[0].money * 2)
-      sql = `UPDATE users SET money = ${rows[0].money * 2} WHERE id = '${message.author.id}'`
-      connection.query(sql)
-    } else {
-      message.channel.send('도박 실패 ㅅㄱ 남은 돈: ' + 0)
-      sql = `UPDATE users SET money = 0 WHERE id = '${message.author.id}'`
-      connection.query(sql)
+    if (err) console.log(err)
+    if (!rows.length) message.channel.send(new MessageEmbed().addField('가입 먼저 해', '`$가입` 하라고요'))
+    else {
+      sql = `SELECT * FROM users WHERE id like '${message.author.id}'`
+      connection.query(sql, (err, rows, fields) => {
+        if (Math.floor(Math.random() * Math.floor(2))) {
+          message.channel.send('도박 성공 ㅅㄱ 남은 돈: ' + rows[0].money * 2)
+          sql = `UPDATE users SET money = ${rows[0].money * 2} WHERE id = '${message.author.id}'`
+          connection.query(sql)
+        } else {
+          message.channel.send('도박 실패 ㅅㄱ 남은 돈: ' + 0)
+          sql = `UPDATE users SET money = 0 WHERE id = '${message.author.id}'`
+          connection.query(sql)
+        }
+      })
     }
-    console.log()
   })
+  
 }
 
 module.exports.help = {
