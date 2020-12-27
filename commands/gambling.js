b369d43ee238ae52a9492687e813d0e91e344a08
시혁 박 <exon.indiv@gmail.com>
2020-12-27 20:47:17 +0900

    도박 크기 정하기

--- commands/gambling.js
+++ commands/gambling.js
@@ -5,14 +5,20 @@ exports.run = async (client, message, args) => {
   const rows = await db('users').select('*').where('id', message.author.id)
   if (!rows.length) message.channel.send(new MessageEmbed().addField('가입 먼저 해', '`$가입` 하라고요'))
   else {
-    const { money } = (await db('users').select('*').where('id', message.author.id))[0]
-    if (Math.floor(Math.random() * Math.floor(2))) {
-      message.channel.send('도박 성공 ㅅㄱ 남은 돈: ' + (money * 2))
-      await db('users').update({ money : money * 2 }).where('id', message.author.id)
-    } else {
-      message.channel.send('도박 실패 ㅅㄱ 남은 돈: ' + 0)
-      await db('users').update({ money : 0 }).where('id', message.author.id)
-    }
+    const gamblingMoney = Number(args)
+    console.log(gamblingMoney)
+    if (gamblingMoney) {
+      const { money } = (await db('users').select('*').where('id', message.author.id))[0]
+      if (gamblingMoney < money) {
+        if (Math.floor(Math.random() * Math.floor(2))) {
+          message.channel.send('도박 성공 ㅅㄱ 남은 돈: ' + (gamblingMoney + money))
+          await db('users').update({ money : gamblingMoney + money }).where('id', message.author.id)
+        } else {
+          message.channel.send('도박 실패 ㅅㄱ 남은 돈: ' + (money - gamblingMoney))
+          await db('users').update({ money : money - gamblingMoney }).where('id', message.author.id)
+        }
+      } else message.channel.send('도박할 돈도 없으면서 도박하지 마')
+    } else message.channel.send('`$도박 [도박할 돈]`')
   }
 }
 
--- events/onMessage.js
+++ events/onMessage.js
@@ -1,5 +1,4 @@
 const Query = require('../classes/Query')
-const { MessageEmbed } = require('discord.js')
 
 module.exports = (client, msg) => {
   if (msg.author.bot) return
@@ -7,10 +6,7 @@ module.exports = (client, msg) => {
 
   const query = new Query(client.settings.prefix, msg.content)
   const target = client.commands.find((c) => c.help.alias.includes(query.cmd) || c.help.name === query.cmd)
-
-  if (target.help.authority === 'Developer' && !client.settings.developers.includes(msg.author.id)) {
-    return msg.channel.send(new MessageEmbed({ title: ':x: Access Denied!', color: 0xFF6961 }))
-  }
+  console.log(target)
 
   if (target) target.run(client, msg, query.args)
 }
