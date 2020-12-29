62b48f84db62ec8ad6e1f019524e2428c32d86bc
시혁 박 <exon.indiv@gmail.com>
2020-12-29 12:55:59 +0900

    fix bug

--- commands/gambling.js
+++ commands/gambling.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
   if (!rows.length) message.channel.send(new MessageEmbed().addField('가입 먼저 해', '`$가입` 하라고요'))
   else {
     const gamblingMoney = Number(args)
-    if (gamblingMoney && gamblingMoney > 0) {
+    if (gamblingMoney && gamblingMoney > 0 && Number.isInteger(gamblingMoney)) {
       const { money } = (await db('users').select('*').where('id', message.author.id))[0]
       if (gamblingMoney <= money) {
         if (Math.floor(Math.random() * Math.floor(2))) {
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
           await db('users').update({ money : money - gamblingMoney }).where('id', message.author.id)
         }
       } else message.channel.send('도박할 돈도 없으면서 도박하지 마 ||니가 폐인이냐?||')
-    } else message.channel.send('`$도박 [도박할 돈]`')
+    } else message.channel.send('`$도박 [도박할 돈]` 소수 음수 안됨')
   }
 }
 
